1d509fa1f65585ed8c6af06f1ed50bea32abe803
Katsiaryna Herasimovich <katrecat33@gmail.com>
2022-11-05 00:33:26 +0100

    [#5] client.cpp: Add mechanism to retrieve messages from server

--- client.cpp
+++ client.cpp
@@ -2,6 +2,12 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
+    fd_set serverfds;
+    struct timeval timeout;
+    timeout.tv_sec = 0;
+    timeout.tv_usec = 100;
+    FD_ZERO(&serverfds);
+
     int client = socket(AF_INET, SOCK_STREAM, 0);
     
     if (argc < 2){
@@ -14,10 +20,21 @@ int main(int argc, char *argv[]) {
     con_info.sin_addr.s_addr = INADDR_ANY;
     connect(client, (sockaddr *)&con_info, sizeof(con_info));
 
+    FD_SET(client, &serverfds);
+    int maxfd = client;
+    char buf[32];
+    send(client, argv[1], 32*sizeof(char), 0);
     while(1)
     {
         send(client, argv[1], 32*sizeof(char), 0);
         sleep(3);
+        select(maxfd+1, &serverfds, NULL, NULL, &timeout);
+        for (int i=0; i <= maxfd; i++)
+            if (FD_ISSET(i, &serverfds))
+            {
+                recv(client, buf, INPUT_BUFFER_SIZE, 0);
+                printf("%s\n", buf);
+            }
     }
     shutdown(client, SHUT_RDWR);
     close(client);
